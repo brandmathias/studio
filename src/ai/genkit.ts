f18734407651ea873be6532147161a69478b3258
Savio Palendeng <saviopalendeng506@gmail.com>
2025-09-27 20:47:33 +0000

    Coba dibantu lagi atasi masalah ini, karena model sudah di update pada g

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: 'AIzaSyAewLP0x-jq1njPK8532BVf2PBnCfIQ1Zo',
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash-latest',
 });
